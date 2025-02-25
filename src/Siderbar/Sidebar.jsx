29af6c21a04ad3d40155b4d30404d0ff52650fcf
Elis Muldaka <emuldaka@gmail.com>
2025-02-24 20:00:27 -0600

    initial commit

---
+++ src/Siderbar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <>
+      <sec className="sidebar">
+        <div className="logo-container">
+          <h1>🛒</h1>
+        </div>
+      </sec>
+    </>
+  );
+}
+
+export default Sidebar;
